e35978424b271d87b77035f61c4404af44ccf61c
Sam Cormack <sam.cormack@gmail.com>
2021-10-10 23:03:23 +1300

    Started class var decs

--- compilationengine/compilationengine.go
+++ compilationengine/compilationengine.go
@@ -2,7 +2,9 @@ package compilationengine
 
 import (
 	"bufio"
+	"fmt"
 	"jackanalyzer/jacktokenizer"
+	"log"
 	"os"
 )
 
@@ -23,17 +25,102 @@ func (engine *CompilationEngine) Finish() {
 }
 
 func (engine *CompilationEngine) CompileClass() {
+	engine.writer.WriteString("<class>\n")
+	// Check class keyword
 	if engine.tokenizer.HasMoreTokens() {
 		engine.tokenizer.Advance()
 
 		if engine.tokenizer.TokenType() == jacktokenizer.KEYWORD &&
 			engine.tokenizer.KeyWord() == jacktokenizer.CLASS {
-			engine.writer.WriteString("Class")
+			engine.writer.WriteString("<keyword> class </keyword>\n")
+		} else {
+			log.Fatal("Did not find expected class keyword.")
 		}
 	}
+
+	// Get class name
+	if engine.tokenizer.HasMoreTokens() {
+		engine.tokenizer.Advance()
+
+		if engine.tokenizer.TokenType() == jacktokenizer.IDENTIFIER {
+			engine.printIdentifier(engine.tokenizer.Identifier())
+		} else {
+			log.Fatal("Did not find an identifier for class.")
+		}
+	}
+
+	// Check {
+	if engine.tokenizer.HasMoreTokens() {
+		engine.tokenizer.Advance()
+
+		if engine.tokenizer.TokenType() == jacktokenizer.SYMBOL &&
+			engine.tokenizer.Symbol() == '{' {
+			engine.printSymbol('{')
+		} else {
+			log.Fatal("Did not find expected { after class declaration.")
+		}
+	}
+
+	// compile ClassVarDec's
+
+	for engine.tokenizer.HasMoreTokens() {
+		engine.tokenizer.Advance()
+
+		if engine.tokenizer.TokenType() == jacktokenizer.KEYWORD &&
+			(engine.tokenizer.KeyWord() == jacktokenizer.STATIC || engine.tokenizer.KeyWord() == jacktokenizer.FIELD) {
+			engine.compileClassVarDec()
+		} else {
+			break
+		}
+	}
+
+	//compile subroutineDec's
+	for engine.tokenizer.HasMoreTokens() {
+		engine.tokenizer.Advance()
+
+		if engine.tokenizer.TokenType() == jacktokenizer.KEYWORD &&
+			(engine.tokenizer.KeyWord() == jacktokenizer.CONSTRUCTOR ||
+				engine.tokenizer.KeyWord() == jacktokenizer.FUNCTION ||
+				engine.tokenizer.KeyWord() == jacktokenizer.METHOD) {
+			engine.compileSubroutine()
+		} else {
+			break
+		}
+	}
+
+	// Check }
+	if engine.tokenizer.HasMoreTokens() {
+		engine.tokenizer.Advance()
+
+		if engine.tokenizer.TokenType() == jacktokenizer.SYMBOL &&
+			engine.tokenizer.Symbol() == '}' {
+			engine.printSymbol('}')
+		} else {
+			engine.writer.Flush()
+			log.Fatal("Did not find expected } after class declaration.")
+		}
+	}
+
+	engine.writer.WriteString("</class>")
+
 }
 
-func (engine *CompilationEngine) compileClassVarDec() {}
+func (engine *CompilationEngine) compileClassVarDec() {
+	engine.writer.WriteString("<classVarDec>")
+	engine.printKeyword(engine.tokenizer.KeyWordName())
+
+	// Type dec
+	if engine.tokenizer.HasMoreTokens() {
+		engine.tokenizer.Advance()
+		if engine.isType() {
+			engine.printKeyword(engine.tokenizer.KeyWordName())
+		} else {
+			log.Fatal("Did not find type declaration for class variable.")
+		}
+	}
+
+	engine.writer.WriteString("</classVarDec>")
+}
 
 func (engine *CompilationEngine) compileSubroutine() {}
 
@@ -58,3 +145,41 @@ func (engine *CompilationEngine) compileExpression() {}
 func (engine *CompilationEngine) compileTerm() {}
 
 func (engine *CompilationEngine) compileExpressionList() {}
+
+func (engine *CompilationEngine) isType() bool {
+	if engine.tokenizer.TokenType() == jacktokenizer.KEYWORD {
+		return engine.tokenizer.KeyWord() == jacktokenizer.INT ||
+			engine.tokenizer.KeyWord() == jacktokenizer.CHAR ||
+			engine.tokenizer.KeyWord() == jacktokenizer.BOOLEAN
+	} else {
+		return engine.tokenizer.TokenType() == jacktokenizer.IDENTIFIER
+	}
+
+}
+
+func (engine *CompilationEngine) printSymbol(r rune) {
+	engine.writer.WriteString(
+		fmt.Sprintf(
+			"<symbol> %s </symbol>\n",
+			string(r),
+		),
+	)
+}
+
+func (engine *CompilationEngine) printIdentifier(s string) {
+	engine.writer.WriteString(
+		fmt.Sprintf(
+			"<identifier> %s </identifier>\n",
+			s,
+		),
+	)
+}
+
+func (engine *CompilationEngine) printKeyword(s string) {
+	engine.writer.WriteString(
+		fmt.Sprintf(
+			"<keyword> %s </keyword>\n",
+			s,
+		),
+	)
+}
